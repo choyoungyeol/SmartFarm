0a30fabf78d8e8af2d3718d84530c07c3e72d2ed
Young-Yeol Cho <60500365+choyoungyeol@users.noreply.github.com>
2021-11-02 22:32:02 +0900

    Create ESP8266_DHT11.ino

---
+++ ESP8266_DHT11.ino
@@ -0,0 +1,99 @@
+#include "WiFiEsp.h"
+#include "DHT.h"
+#define DHTPIN 12
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+char ssid[] = "********";            // your network SSID (name)
+char pass[] = "********";        // your network password
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
+WiFiEspServer server(80);
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial2.begin(9600);
+  WiFi.init(&Serial2);
+
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    while (true);
+  }
+
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+  }
+
+  Serial.println("You're connected to the network");
+  printWifiStatus();
+
+  server.begin();
+}
+
+
+void loop()
+{
+  int humidity = dht.readHumidity();
+  int temperature = dht.readTemperature();
+  // listen for incoming clients
+  WiFiEspClient client = server.available();
+  if (client) {
+    Serial.println("New client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        if (c == '\n' && currentLineIsBlank) {
+          Serial.println("Sending response");
+          client.print(
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/html\r\n"
+            "Connection: close\r\n"
+            "Refresh: 20\r\n"
+            "\r\n");
+          client.print("<!DOCTYPE HTML>\r\n");
+          client.print("<html>\r\n");
+          client.print("<h1>Greenhouse Environment</h1>\r\n");
+          client.print("Temperature : ");
+          client.print(temperature);
+          client.print(" oC");
+          client.print("<br>\r\n");
+          client.print("Humidity : ");
+          client.print(humidity);
+          client.print(" %");
+          client.print("<br>\r\n");
+          client.print("</html>\r\n");
+          break;
+        }
+        if (c == '\n') {
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r') {
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    delay(10);
+    client.stop();
+    Serial.println("Client disconnected");
+  }
+}
+
+void printWifiStatus()
+{
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  Serial.println();
+  Serial.print("To see this page in action, open a browser to http://");
+  Serial.println(ip);
+  Serial.println();
+}
